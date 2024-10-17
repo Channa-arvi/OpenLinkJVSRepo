02b23a0c7c6f8ea92e32259acf259bb46bf81a4d
Channa-arvi <channa.arvi@gmail.com>
2024-10-17 16:36:33 +0800

    Added 2 jvs files
    Signed-off-by: Channa-arvi <channa.arvi@gmail.com>

---
+++ JVS/SimpleJVSScript2.java
@@ -0,0 +1,34 @@
+package com.customer;
+
+import com.garinternal.common.util.GarLogger;
+import com.olf.embedded.application.EnumScriptCategory;
+import com.olf.embedded.application.ScriptCategory;
+import com.olf.embedded.generic.AbstractGenericScript;
+import com.olf.openjvs.OException;
+import com.olf.openjvs.PluginCategory;
+import com.olf.openjvs.PluginType;
+import com.olf.openjvs.Table;
+import com.olf.openjvs.enums.SCRIPT_CATEGORY_ENUM;
+import com.olf.openjvs.enums.SCRIPT_TYPE_ENUM;
+
+@ScriptCategory({ EnumScriptCategory.Generic })
+@PluginCategory(SCRIPT_CATEGORY_ENUM.SCRIPT_CAT_GENERIC)
+@PluginType(SCRIPT_TYPE_ENUM.MAIN_SCRIPT)// TODO select script type here)
+
+public class SimpleJVSScript2 extends AbstractGenericScript {
+    private GarLogger logger;
+
+    /**
+     * Constructor
+     *
+     * @throws OException {@link OException}
+     */
+    public SimpleJVSScript2() throws OException {
+        super();
+        // this.logger = super.getLoggerInstance();
+    }
+
+    public void execute(Table argt, Table returnt) throws OException {
+        // TODO: Start writing code here
+    }
+}
