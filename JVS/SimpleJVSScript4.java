246d300e320f56663a2dddb25f27e4df76ab18ef
Channa-arvi <channa.arvi@gmail.com>
2024-10-18 23:47:17 +0800

    Signed-off-by: Channa-arvi <channa.arvi@gmail.com>

--- JVS/SimpleJVSScript4.java
+++ JVS/SimpleJVSScript4.java
@@ -48,6 +48,5 @@ public class SimpleJVSScript4 extends AbstractGenericScript {
 
     public void execute(Table argt, Table returnt) throws OException {
         // TODO: Start writing code here
-		OConsole.oprint("Weather is very good ");
     }
 }
